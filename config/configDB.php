6a8fe16bbc4453d3d2895696192fb49f1ce258f3
Miguel Angel <miguela.aragar@educa.jcyl.es>
2021-01-18 21:15:59 +0100

    Cambio de adaptador Db

--- config/configDB.php
+++ config/configDB.php
@@ -2,22 +2,22 @@
 //-------------------------------------Base--------------------------------------------------
 /*define("DNS","mysql:host=ip servidor;dbname=nombre base de datos");
 define("USER","nombre usuario");
-define("PASSWORD","Contraseña"); 
-/
+define("PASSWORD","Contraseña"); */
+
 
 //-------------------------------------Heracio--------------------------------------------------
 //conexion 1 and 1
-/*define("DNS","mysql:host=db771560945.hosting-data.io;dbname=db771560945"); //Variable con el servidor y la base de datos
+define("DNS","mysql:host=db771560945.hosting-data.io;dbname=db771560945"); //Variable con el servidor y la base de datos
 define("USER","dbo771560945"); //Usuario administrador de esa base de datos
-define("PASSWORD","Covid1234$"); //Contraseña del usuario*/
+define("PASSWORD","Covid1234$"); //Contraseña del usuario
 
 
 
 //-------------------------------------Desarrollo Casa--------------------------------------------------
 //no olvidar comentar el hostblind de mysql en su archivo de configuración
-define('DNS',"mysql:host=192.168.33.20;dbname=DAW210DBProyectoTema5");
+/*define('DNS',"mysql:host=192.168.33.20;dbname=DAW210DBProyectoTema5");
 define('USER',"usuarioDAW210DBProyectoTema5");
-define('PASSWORD',"paso");
+define('PASSWORD',"paso");*/
 
 //-------------------------------------Desarrollo clase--------------------------------------------------
 //no olvidar comentar el hostblind de mysql en su archivo de configuración
